e76a351f4f0a090e6cd61fa8e479235df077518d
bs1462 <jobaer.ahamed@brainstation-23.com>
2024-02-05 19:08:12 +0600

    add comments

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 # Create your views here.
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    template_name = 'accounts/signup.html'
-    
+# class SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     template_name = 'accounts/signup.html'
+
+def SignUpView(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog/registration/login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
